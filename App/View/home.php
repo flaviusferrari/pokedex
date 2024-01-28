652b7b7eeeff031fe6779b1dc01ddf357facdc19
Flavius Ferrari <flaviusferrari@gmail.com>
2024-01-28 07:52:14 -0300

    Ajustando o layout

--- App/View/home.php
+++ App/View/home.php
@@ -3,12 +3,14 @@
 <h2>Digite o nome de um Pokemon</h2>
 
 <form action="/pokemon" method="post">
-    <label for="nome">Nome do Pokemon</label>
-    <input type="text" name="pokemon" id="">
+    <div class="form-group">
+        <label for="nome">Nome do Pokemon</label>
+        <input type="text" class="form-control" name="pokemon">        
+    </div>
 
-    <button type="submit">Enviar</button>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    <a class="btn btn-success" href="pokemon/list">Listar</a>
 </form>
 <hr>
-<a href="pokemon/list">Listar</a>
 
 <?php view('layout/footer'); ?>
--- App/View/pokemon/dados_pokemon.php
+++ App/View/pokemon/dados_pokemon.php
@@ -1,27 +1,51 @@
 <?php view('layout/header'); ?>
 
-Dados do Pokemon desejado: 
-<br>
-
-Nome: <?= $pokemon->name; ?><br>
-Foto: <?= $pokemon->sprites->front_default; ?><br>
-Peso: <?= $pokemon->weight; ?><br>
-Altura: <?= $pokemon->height; ?><br>
-Tipos: 
-<?php $types = ''; ?>
-<?php foreach ($pokemon->types as $item): ?>
-    <?php $types .= $item->type->name . '|' ?>
-    <?= $item->type->name; ?> - 
-<?php endforeach; ?>
+<h1>Dados do Pokemon desejado</h1> 
 
 <form action="/pokemon/save" method="post">
-    <input type="hidden" name="nome" value="<?= $pokemon->name; ?>">
-    <input type="hidden" name="foto" value="<?= $pokemon->sprites->front_default; ?>">
-    <input type="hidden" name="peso" value="<?= $pokemon->weight; ?>">
-    <input type="hidden" name="altura" value="<?= $pokemon->height; ?>">
-    <input type="hidden" name="tipo" value="<?= $types; ?>">
-    <button type="submit">Favoritar</button>
+    <div class="row">   
+        <div class="col-1">
+            <img src="<?= $pokemon->sprites->front_default; ?>" alt="">
+            <input type="hidden" name="foto" value="<?= $pokemon->sprites->front_default; ?>">
+        </div>
+        <div class="col-11">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="nome">Nome</label><br>
+                        <input type="text" class="form-control" name="nome" value="<?= $pokemon->name; ?>" readonly>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Peso</label>
+                        <input type="text" class="form-control" name="peso" value="<?= $pokemon->weight; ?>" readonly>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Altura</label>
+                        <input type="text" class="form-control" name="altura" value="<?= $pokemon->height; ?>" readonly>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+            <?php foreach ($pokemon->types as $item): ?>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Tipo</label>
+                        <input type="text" class="form-control" name="tipo[]" value="<?= $item->type->name; ?>" readonly>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            </div>
+            <div class="row">
+                <button class="btn btn-primary" type="submit">Favoritar</button>&nbsp;
+                <a class="btn btn-success" href="/">Home</a>
+            </div>
+        </div>
+    </div>
 </form>
-<a href="/">Home</a>
+
 
 <?php view('layout/footer'); ?>
